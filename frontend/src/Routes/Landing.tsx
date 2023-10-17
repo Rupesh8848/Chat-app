5857908ad567b48340f7f1fd347fe44475a2a226
Anish Shahi <ashahi322@gmail.com>
2023-10-17 10:14:04 +0545

    Landing UI

--- frontend/src/Routes/Landing.tsx
+++ frontend/src/Routes/Landing.tsx
@@ -11,15 +11,23 @@ export default function Landing({
 }) {
   return (
     <>
-      <div>
-        Enter your username:
-        <input
-          type="text"
-          className="border-2 border-black"
-          onChange={handleChange}
-          value={userName || ""}
-        />
-        <button onClick={handleLogin}>Login</button>
+      <div className="container mx-auto h-screen flex flex-col justify-center items-center ">
+        <h1 className="text-4xl font-bold mb-8">Chat-Rat</h1>
+        <div className="flex flex-col items-center">
+          <label className="text-lg mb-2">Enter your username:</label>
+          <input
+            type="text"
+            className="border-2 border-black px-4 py-2 rounded-lg mb-4"
+            onChange={handleChange}
+            value={userName || ""}
+          />
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+        </div>
       </div>
     </>
   );
